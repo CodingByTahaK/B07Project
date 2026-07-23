75b0d45c4c80998356820a3e3137e71667c12d43
Kavya Patel <patelkavya.1225@gmail.com>
2026-07-23 19:42:29 -0400

    fix/resolved-issues

--- app/src/main/java/com/cscb07/museum/HomeFragment.java
+++ app/src/main/java/com/cscb07/museum/HomeFragment.java
@@ -10,8 +10,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class HomeFragment extends Fragment {
     @Nullable
